ff18f26e975290a37fba45b4b6fad8618c664261
Doyeon Kim <90243955+Doyeon-k-fanshawe@users.noreply.github.com>
2022-11-17 12:44:35 -0500

    adding Slide Function

--- js/main.js
+++ js/main.js
@@ -2,8 +2,33 @@ import TheCarthumb from "./components/TheCarThumbnail.js";
 import Showbox from "./components/TheShowBox.js";
 
 (() => {
+
+  // Slider
+    
+  let slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+      let slides = document.getElementsByClassName("slides"),
+          dots = document.getElementsByClassName("dot");
+          
+      let i;
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}    
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+      setTimeout(showSlides, 3500); 
+  }
+
   // create vue instance here
   const { createApp } = Vue
+  
 
   createApp({
     created() {
